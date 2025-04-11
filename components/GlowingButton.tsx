d5db5a8396adfa257745824c7422c6348e8c7101
JEGUVEERA <139042611+JEGUVEERA@users.noreply.github.com>
2025-04-11 14:59:40 +0530

    Delete components/GlowingButton.tsx

--- components/GlowingButton.tsx
+++
@@ -1,9 +0,0 @@
-// components/GlowingButton.tsx
-export default function GlowingButton() {
-    return (
-      <button className="mt-6 px-6 py-3 bg-purple-800 hover:bg-purple-700 text-white font-semibold rounded-md shadow-lg transition transform hover:scale-105 glow-button">
-        GET A QUOTE
-      </button>
-    );
-  }
-  
