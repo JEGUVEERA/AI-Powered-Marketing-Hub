92c7a54b255332acafc43bdc33582f48a6440100
JEGUVEERA <jagaddisshmediboina561@gmail.com>
2025-04-24 01:34:24 +0530

    update file code lines

--- components/NeonProgressBar.tsx
+++
@@ -1,9 +0,0 @@
-// components/NeonProgressBar.tsx
-export default function NeonProgressBar() {
-    return (
-      <div className="w-full h-2 bg-gray-900 rounded-full overflow-hidden">
-        <div className="h-full bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 neon-progress-bar"></div>
-      </div>
-    )
-  }
-  
